f14e76012918f8c4877c7df36872931eba73f4a6
melvinkj <melvinkentj@gmail.com>
2023-02-24 13:22:23 +0700

    feat: visualize points

--- src/BruteForce.py
+++ src/BruteForce.py
@@ -1,5 +1,8 @@
 import random
 import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 def generateRandomPoints(total_points) :
     points = [[0, 0, 0] for i in range(total_points)]
@@ -14,6 +17,27 @@ def generateRandomPoints(total_points) :
 def getDistance(p1, p2):
     distance = math.sqrt(((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2))
     return distance
+
+# def sorting(points) :
+
+
+def visualize(points, p1, p2) :
+    # length = len(points)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    for x, y, z in points:
+        if(x == p1[0] and y == p1[1] and z == p1[2]) or (x == p2[0] and y == p2[1] and z == p2[2]) :
+            ax.scatter(x,y,z, c='b', marker='o')
+        else:
+            ax.scatter(x,y,z, c='r', marker='o')
+
+    plt.show()
 
 def main():
     points = generateRandomPoints(10)
@@ -34,5 +58,7 @@ def main():
     print("The shortest distance is", shortestDistance )
     print("P1:", p1)
     print("P2: ", p2)
+
+    visualize(points, p1, p2)
 
 main()
