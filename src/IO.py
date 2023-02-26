93d1fa462acea18147ae8be21762540952a16ad5
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-02-26 20:51:18 +0700

    feat(IO): add menu, read from file

--- src/IO.py
+++ src/IO.py
@@ -1,13 +1,56 @@
 import random
 import math
+import os
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def generateRandomPoints(total_points) :
-    points = [[0, 0, 0] for i in range(total_points)]
+def splashScreen():
+    print("Welcome to closest pair program :D\n")
+
+def mainMenu():
+    print("""
+        Choose:
+        1. Generate random points
+        2. File input
+        3. Exit
+    """)
+
+def algorithmMenu():
+    print("""
+        Choose the algorithm to find closest pair:
+        1. Divide and Conquer
+        2. Brute Force
+    """)
+
+# TODO: Input validation gatau masih kurang atau ga
+def inputHandler():
+    while True:
+        nPoints = int(input("Enter the n points you want to generate: "))
+        if (nPoints >= 2):
+            break
+        print("Your input of n points is not valid. Please input a number with a minimum of two!")
+    while True:
+        nDims = int(input("Enter the d dimension of points you want to generate: "))
+        if (nDims > 0):
+            break
+        print("Your input of d dimensions is not valid. Please input a number with a minimum of one!")
+    return nPoints, nDims
+
+# TODO: Input from file validation? blm tau perlu atau ga
+def readFromFile(fileName):
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    f = open(dir_path + "\\test\\" + fileName + ".txt", "r")
+    lines = f.readlines()
+    points = [list(map(int, line.strip().split())) for line in lines]
+    print(points)
+    f.close()
+    return points
+
+def generateRandomPoints(total_points, total_dim) :
+    points = [[0 for j in range (total_dim)] for i in range(total_points)]
 
     for i in range (total_points) :
-        for j in range (3) :
+        for j in range (total_dim) :
             points[i][j] = random.randint(-100, 100)
     
     print(points)
