19afbdd0442fa3ab0b69f7a7d70d0c9707fbc1e5
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-02-27 23:01:34 +0700

    feat: add unique append and extend

--- src/Utils.py
+++ src/Utils.py
@@ -6,3 +6,23 @@ def getDistancePoints(point1, point2):
         dist += (point1[i] - point2[i]) ** 2
     return math.sqrt(dist)
 
+def appendIfNotSame(closestPairs, pointPair):
+    has_common_element = False
+    for arr in closestPairs:
+        if (pointPair == [[]]):
+            has_common_element = True
+            break
+        if ((pointPair[0] == arr[0] or pointPair[0] == arr[1]) and (pointPair[1] == arr[0] or pointPair[1] == arr[1])):
+            has_common_element = True
+            break
+
+    # Melakukan append hanya jika pasangan point pada pointPair tidak sama dengan salah satu pasangan pada closestPairs
+    if not has_common_element:
+        closestPairs.append(pointPair)
+    return closestPairs
+
+def extendIfNotSame(closestPairs, pointPairs):
+    for arr in pointPairs:
+        closestPairs = appendIfNotSame(closestPairs, arr)
+
+    return closestPairs
